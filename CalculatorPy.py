7ad0abcd3f2cca6d67e158dd6485ef82dd2a818d
DoctorStrange69 <72389227+DoctorStrange69@users.noreply.github.com>
2025-02-01 09:09:41 +0400

    TestCalculator.py

--- CalculatorPy.py
+++ CalculatorPy.py
@@ -1,20 +1,75 @@
 import unittest
-from Calculator import Calculator
+from unittest.mock import patch
+from calculate import Calculator
+import io
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 class TestCalculator(unittest.TestCase):
 
+    def setUp(self):
+        logging.info("\nЗапуск нового теста...")
 
-  def setUp(self):
-    self.calculator = Calculator()
-  def test_add(self):
-    self.assertEqual(self.calculator.add(4,7), 11)
-  def test_subtract(self):
-    self.assertEqual(self.calculator.subtract(10,5), 5)
-  def test_multiply(self):
-    self.assertEqual(self.calculator.multiply(3,7), 21)
-  def test_divide(self):
-    self.assertEqual(self.calculator.divide(10,2), 5)
+    @patch('builtins.input', return_value='4')
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_add(self, mock_stdout, mock_input):
+        logging.info("Тест сложения: 4 + 4")
+        Calculator.add(4)
+        result = mock_stdout.getvalue().strip()
+        logging.info(f"Результат: {result}")
 
+    @patch('builtins.input', return_value='5')
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_subtract(self, mock_stdout, mock_input):
+        logging.info("Тест вычитания: 10 - 5")
+        Calculator.subtract(10)
+        result = mock_stdout.getvalue().strip()
+        logging.info(f"Результат: {result}")
 
-if __name__ == "__main__":
-  unittest.main()
+    @patch('builtins.input', return_value='2')
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_divide(self, mock_stdout, mock_input):
+        logging.info("Тест деления: 10 / 2")
+        Calculator.divide(10)
+        result = mock_stdout.getvalue().strip()
+        logging.info(f"Результат: {result}")
+
+        with patch('builtins.input', return_value='0'):
+            logging.info("Тест деления на ноль")
+            try:
+                Calculator.divide(5)
+            except ValueError as e:
+                result = "Ошибка: " + str(e)
+                logging.info(f"Результат: {result}")
+            else:
+                logging.info("Ожидается ошибка при делении на ноль.")
+
+        with patch('builtins.input', return_value='5'):
+            logging.info("Тест деления на ненулевое число")
+            try:
+                Calculator.divide(5)
+            except ValueError as e:
+                result = "Ошибка: " + str(e)
+                logging.info(f"Результат: {result}")
+            else:
+                logging.info("Ожидается нормальный вывод в stdout.")
+
+    @patch('builtins.input', return_value='5')
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_multiply(self, mock_stdout, mock_input):
+        logging.info("Тест умножения: 3 * 5")
+        Calculator.multiply(3)
+        result = mock_stdout.getvalue().strip()
+        logging.info(f"Результат: {result}")
+
+    @patch('builtins.input', return_value='3')
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_degree(self, mock_stdout, mock_input):
+        logging.info("Тест возведения в степень: 2 ^ 3")
+        Calculator.power(2)
+        result = mock_stdout.getvalue().strip()
+        logging.info(f"Результат: {result}")
+
+if __name__ == '__main__':
+    unittest.main(verbosity=1, buffer=False)
